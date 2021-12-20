78102f86883df9617885ec78bda33012d3c3ff03
Itxaka <igarcia@suse.com>
2021-12-20 15:43:01 +0100

    Move grub tests to ginkgo
    Signed-off-by: Itxaka <igarcia@suse.com>

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -17,12 +17,15 @@ limitations under the License.
 package utils_test
 
 import (
+	"bytes"
 	"errors"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/rancher-sandbox/elemental-cli/pkg/constants"
 	v1 "github.com/rancher-sandbox/elemental-cli/pkg/types/v1"
 	"github.com/rancher-sandbox/elemental-cli/pkg/utils"
 	v1mock "github.com/rancher-sandbox/elemental-cli/tests/mocks"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/afero"
 	"k8s.io/mount-utils"
 	"testing"
@@ -148,6 +151,103 @@ var _ = Describe("Utils", func() {
 			out, err := utils.FindLabel(runner, "FAKE")
 			Expect(err).ToNot(BeNil())
 			Expect(out).To(Equal(""))
+		})
+	})
+	Context("Grub", func() {
+		Context("Install", func() {
+			BeforeEach(func() {
+				config.Device = "/dev/test"
+				config.StateDir = "/state"
+			})
+			It("installs with default values", func() {
+				buf := &bytes.Buffer{}
+				logger := log.New()
+				logger.SetOutput(buf)
+
+				_ = fs.MkdirAll("/state/grub2/", 0666)
+				_ = fs.MkdirAll("/etc/cos/", 0666)
+				err := afero.WriteFile(fs, "/etc/cos/grub.cfg", []byte("console=tty1"), 0644)
+				Expect(err).To(BeNil())
+
+				config.Logger = logger
+				config.Device = "/dev/test"
+				config.StateDir = "/state"
+				config.GrubConf = "/etc/cos/grub.cfg"
+
+				grub := utils.NewGrub(config)
+				err = grub.Install()
+				Expect(err).To(BeNil())
+
+				Expect(buf).To(ContainSubstring("Installing GRUB.."))
+				Expect(buf).To(ContainSubstring("Grub install to device /dev/test complete"))
+				Expect(buf).ToNot(ContainSubstring("efi"))
+				Expect(buf.String()).ToNot(ContainSubstring("Adding extra tty (serial) to grub.cfg"))
+				targetGrub, err := afero.ReadFile(fs, "/state/grub2/grub.cfg")
+				Expect(err).To(BeNil())
+				// Should not be modified at all
+				Expect(targetGrub).To(ContainSubstring("console=tty1"))
+
+			})
+			It("installs with efi on efi system", func() {
+				buf := &bytes.Buffer{}
+				logger := log.New()
+				logger.SetOutput(buf)
+				logger.SetLevel(log.DebugLevel)
+				_, _ = fs.Create("/etc/cos/grub.cfg")
+				_, _ = fs.Create(constants.EfiDevice)
+
+				config.Logger = logger
+
+				grub := utils.NewGrub(config)
+				err := grub.Install()
+				Expect(err).To(BeNil())
+
+				Expect(buf.String()).To(ContainSubstring("--target=x86_64-efi"))
+				Expect(buf.String()).To(ContainSubstring("--efi-directory"))
+				Expect(buf.String()).To(ContainSubstring("Installing grub efi for arch x86_64"))
+			})
+			It("installs with efi with --force-efi", func() {
+				buf := &bytes.Buffer{}
+				logger := log.New()
+				logger.SetOutput(buf)
+				logger.SetLevel(log.DebugLevel)
+				_, _ = fs.Create("/etc/cos/grub.cfg")
+
+				config.Logger = logger
+				config.ForceEfi = true
+
+				grub := utils.NewGrub(config)
+				err := grub.Install()
+				Expect(err).To(BeNil())
+
+				Expect(buf.String()).To(ContainSubstring("--target=x86_64-efi"))
+				Expect(buf.String()).To(ContainSubstring("--efi-directory"))
+				Expect(buf.String()).To(ContainSubstring("Installing grub efi for arch x86_64"))
+			})
+			It("installs with extra tty", func() {
+				buf := &bytes.Buffer{}
+				logger := log.New()
+				logger.SetOutput(buf)
+				_ = fs.MkdirAll("/state/grub2/", 0666)
+				_ = fs.MkdirAll("/etc/cos/", 0666)
+				err := afero.WriteFile(fs, "/etc/cos/grub.cfg", []byte("console=tty1"), 0644)
+				Expect(err).To(BeNil())
+				_, _ = fs.Create("/dev/serial")
+
+				config.Logger = logger
+				config.Tty = "serial"
+				config.GrubConf = "/etc/cos/grub.cfg"
+
+				grub := utils.NewGrub(config)
+				err = grub.Install()
+				Expect(err).To(BeNil())
+
+				Expect(buf.String()).To(ContainSubstring("Adding extra tty (serial) to grub.cfg"))
+				targetGrub, err := afero.ReadFile(fs, "/state/grub2/grub.cfg")
+				Expect(err).To(BeNil())
+				Expect(targetGrub).To(ContainSubstring("console=tty1 console=serial"))
+
+			})
 		})
 	})
 })
