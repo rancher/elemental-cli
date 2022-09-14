757ca34c019d5d5d89389bbcfce866af082d29f7
Itxaka <igarcia@suse.com>
2022-09-14 19:06:05 +0200

    Rework grub config and modules for install
    Not sure where we were getting the grub.cfg before but it wasnt from the
    install source, which is bad.
    
    This patch changes to pick the grub from the install source and copy it
    into the state partition.
    
    It also copies the required grub modules into the state partition as we
    no longer call the grub2-install which used to generate them. As we now
    use the default grub.efi from the shim package, we no longer need to
    generate the efi artifact and we only ship the 2 required extra modules
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- pkg/utils/grub.go
+++ pkg/utils/grub.go
@@ -64,25 +64,24 @@ func (g Grub) Install(target, rootDir, bootDir, grubConf, tty string, efi bool, 
 		g.config.Logger.Infof("Grub install to device %s complete", target)
 	}
 
-	grub1dir := filepath.Join(bootDir, "grub")
-	grub2dir := filepath.Join(bootDir, "grub2")
+	// At this point the active mountpoint has all the data from the installation source, so we should be able to use
+	// the grub.cfg bundled in there
+	grubdir = filepath.Join(rootDir, grubConf)
+	g.config.Logger.Infof("Using grub config dir %s", grubdir)
 
-	// Select the proper dir for grub
-	if ok, _ := IsDir(g.config.Fs, grub1dir); ok {
-		grubdir = grub1dir
-	}
-	if ok, _ := IsDir(g.config.Fs, grub2dir); ok {
-		grubdir = grub2dir
-	}
-	g.config.Logger.Infof("Found grub config dir %s", grubdir)
-
-	grubCfg, err := g.config.Fs.ReadFile(filepath.Join(rootDir, grubConf))
+	grubCfg, err := g.config.Fs.ReadFile(grubdir)
 	if err != nil {
 		g.config.Logger.Errorf("Failed reading grub config file: %s", filepath.Join(rootDir, grubConf))
 		return err
 	}
 
-	grubConfTarget, err := g.config.Fs.Create(fmt.Sprintf("%s/grub.cfg", grubdir))
+	// Create Needed dir under state partition to store the grub.cfg and any needed modules
+	err = MkdirAll(g.config.Fs, filepath.Join(bootDir, fmt.Sprintf("grub2/%s-efi", g.config.Arch)), cnst.DirPerm)
+	if err != nil {
+		return fmt.Errorf("error creating grub dir: %s", err)
+	}
+
+	grubConfTarget, err := g.config.Fs.Create(filepath.Join(bootDir, "grub2/grub.cfg"))
 	if err != nil {
 		return err
 	}
@@ -111,16 +110,34 @@ func (g Grub) Install(target, rootDir, bootDir, grubConf, tty string, efi bool, 
 		finalContent = string(grubCfg)
 	}
 
-	g.config.Logger.Infof("Copying grub contents from %s to %s", grubConf, fmt.Sprintf("%s/grub.cfg", grubdir))
+	g.config.Logger.Infof("Copying grub contents from %s to %s", grubdir, filepath.Join(bootDir, "grub2/grub.cfg"))
 	_, err = grubConfTarget.WriteString(finalContent)
 	if err != nil {
 		return err
 	}
 
 	if efi {
+		// Copy required extra modules to boot dir under the state partition
+		// otherwise if we insmod it will fail to find them
+		// We no longer call grub-install here so the modules are not setup automatically in the state partition
+		// as they were before. We now use the bundled grub.efi provided by the shim package
+		for _, m := range []string{"loopback.mod", "squash4.mod"} {
+			fileReadName := filepath.Join(rootDir, fmt.Sprintf("/usr/share/grub2/%s-efi/%s", g.config.Arch, m))
+			fileWriteName := filepath.Join(bootDir, fmt.Sprintf("grub2/%s-efi/%s", g.config.Arch, m))
+			g.config.Logger.Debugf("Copying %s to %s", fileReadName, fileWriteName)
+			fileContent, err := g.config.Fs.ReadFile(fileReadName)
+			if err != nil {
+				return fmt.Errorf("error reading %s: %s", fileReadName, err)
+			}
+			err = g.config.Fs.WriteFile(fileWriteName, fileContent, cnst.FilePerm)
+			if err != nil {
+				return fmt.Errorf("error writing %s: %s", fileWriteName, err)
+			}
+		}
+
 		if exists, _ := Exists(g.config.Fs, filepath.Join(rootDir, fmt.Sprintf("/usr/share/efi/%s/", g.config.Arch))); exists {
-			g.config.Logger.Infof("Generating grub files for efi", target)
-			err := MkdirAll(g.config.Fs, filepath.Join(cnst.EfiDir, "EFI/elemental/"), cnst.DirPerm)
+			g.config.Logger.Infof("Generating grub files for efi on %s", target)
+			err = MkdirAll(g.config.Fs, filepath.Join(cnst.EfiDir, "EFI/elemental/"), cnst.DirPerm)
 			if err != nil {
 				g.config.Logger.Errorf("Error creating dirs: %s", err)
 				return err
@@ -144,6 +161,8 @@ func (g Grub) Install(target, rootDir, bootDir, grubConf, tty string, efi bool, 
 			}
 
 			// Add grub.cfg in EFI that chainloads the grub.cfg in recovery
+			// Notice that we set the config to /grub2/grub.cfg which means the above we need to copy the file from
+			// the installation source into that dir
 			grubCfgContent := []byte(fmt.Sprintf("search --no-floppy --label --set=root %s\nset prefix=($root)/grub2\nconfigfile ($root)/grub2/grub.cfg", stateLabel))
 			err = g.config.Fs.WriteFile(filepath.Join(cnst.EfiDir, "EFI/elemental/grub.cfg"), grubCfgContent, cnst.FilePerm)
 			if err != nil {
